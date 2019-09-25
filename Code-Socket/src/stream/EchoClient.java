51457d4fed0c67ffa705569cd8fd4c85602edd70
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-09-25 11:09:58 +0200

    re join

--- Code-Socket/src/stream/EchoClient.java
+++ Code-Socket/src/stream/EchoClient.java
@@ -16,31 +16,22 @@ import java.util.Date;
 
 public class EchoClient {
 
-
-    /**
-     * main method
-     * accepts a connection, receives a message from client then sends an echo to the client
-     **/
-    public static void main(String[] args) throws IOException {
+    private static void join(String ipAdress, int port, BufferedReader stdIn) throws IOException {
+        // args[0] args[1]
 
         Socket clientToServerSocket = null;
-        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-        if (args.length != 2) {
-            System.out.println("Usage: java EchoClient <EchoServer host> <EchoServer port>");
-            System.exit(1);
-        }
 
         try {
             // creation socket ==> connexion
-            clientToServerSocket = new Socket(args[0], new Integer(args[1]).intValue());
+            clientToServerSocket = new Socket(ipAdress, port);
             System.out.println("Connected to server " + clientToServerSocket.getInetAddress());
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host:" + args[0]);
+            System.err.println("Don't know about host:" + ipAdress);
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for "
-                    + "the connection to:" + args[0]);
+                    + "the connection to:" + ipAdress);
             System.exit(1);
         }
 
@@ -59,7 +50,31 @@ public class EchoClient {
         ClientReadThread clientReadThread = new ClientReadThread(new ObjectInputStream(clientToServerSocket.getInputStream()));
         clientWriteThread.start();
         clientReadThread.start();
+        while(clientReadThread.isAlive() && clientWriteThread.isAlive()) {
+        }
+    }
 
+    /**
+     * main method
+     * accepts a connection, receives a message from client then sends an echo to the client
+     **/
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.out.println("Usage: java EchoClient <EchoServer host> <EchoServer port>");
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            System.out.println("Pour rejoindre, entrez : join");
+            String option = stdIn.readLine();
+
+            if (option.equals("join")){
+                join(args[0], new Integer(args[1]).intValue(), stdIn);
+
+            }
+        }
     }
 }
 
